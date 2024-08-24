6127178ad7d5f0fb73058e025b062105f553fffb
Raaffss <rafaellavagnini@gmail.com>
2024-08-23 21:21:16 -0300

    Removing unnecessary files

--- TCC/API/monitoramento/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from monitoramento.models import Usuario    
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = '__all__'
--- TCC/API/monitoramento/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from monitoramento.models import Usuario
-from monitoramento.serializer import UsuarioSerializer
-
-class UsuariosViewSet(viewsets.ModelViewSet):
-    """Exibindo todos os usuários"""
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
